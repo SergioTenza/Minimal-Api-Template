b84941f494c9b49566ba4ff070d39a16260424af
Sergio Tenza Lopez <sergeix_tc@hotmail.com>
2022-12-27 14:04:34 +0100

    INFRA: EF Core setup

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,4 +1,14 @@
+using Application.Abstractions;
+using DataAccess;
+using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var cs = builder.Configuration["ConnectionString"];// ?? "Server=localhost,1433;Database=tempdb;User ID=sa;Password=<PlaceholderPassword>;Persist Security Info=False;TrustServerCertificate=true;";
+builder.Services.AddDbContext<SocialDbContext>(opt =>
+    opt.UseSqlServer(cs));
+builder.Services.AddScoped<IPostRepository, PostRepository>();
 
 var app = builder.Build();
 
